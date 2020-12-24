11d87d02f3ef2c5aef5dc7246465482ba85b3f3a
deadln <glebuch2000@list.ru>
2020-12-24 13:44:58 +0300

    Change host and port mechanisms

--- Chat-Netty/src/main/java/NettyClient.java
+++ Chat-Netty/src/main/java/NettyClient.java
@@ -13,8 +13,12 @@ import java.util.Scanner;
 public class NettyClient {
     public static void main(String[] args) throws Exception {
 
-        String host = "localhost";
-        int port = 8080;
+        String host = args.length > 0
+                ? args[0]
+                : "localhost";
+        int port = args.length > 1
+                ? Integer.parseInt(args[1])
+                : 8080;
         EventLoopGroup workerGroup = new NioEventLoopGroup();
 
         try {
